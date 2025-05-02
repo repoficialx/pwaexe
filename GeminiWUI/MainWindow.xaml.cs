d2240b61216b9b6fba6ef85483b3512c97d1cc62
Ray <raytrucos@icloud.com>
2025-05-02 19:32:49 +0200

    GWUI

--- GeminiWUI/MainWindow.xaml.cs
+++ GeminiWUI/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Gemini_WinUI
+namespace GeminiWUI
 {
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
@@ -30,7 +30,6 @@ namespace Gemini_WinUI
 
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
-            //myButton.Content = "Clicked";
         }
     }
 }
